ca1ee6948e6336c9b8222fe26d602de5deffb596
julirmatos <juli.rmatos@gmail.com>
2025-11-05 16:24:00 -0300

    Projeto 01 - Conta Bancária

---
+++ menu.ts
@@ -0,0 +1,17 @@
+import readlineSync = require('readline-sync')
+export function main() {
+    let opcao: number;
+    while (true) {
+        console.log("******************************");
+
+
+
+
+
+
+
+
+
+
+    }
+}
